5a77cc012c81dc42eb7b2b7c7fd85da98660ae64
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-17 22:44:51 +0800

    Update qg1_1.c

--- largegroup/week1/linklist/qg1_1.c
+++ largegroup/week1/linklist/qg1_1.c
@@ -1,4 +1,14 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h> 
+#include <conio.h>
+#include <time.h>                   //
+#define Len sizeof(struct event)    //Len为结构体的长度 
+typedef  struct event Event;        //重命名结构体
+typedef  struct date Date;
+typedef  struct reminder Remd;
+typedef  struct time Time;
 
 int main (){//主函数
      Event *head = getfromfile();
