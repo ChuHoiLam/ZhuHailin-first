1a6b776e7f195cd4aad5fab650484676643df54f
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-18 00:05:50 +0800

    Update qg1_2.c

--- largegroup/week1/dulinklist/qg1_2.c
+++ largegroup/week1/dulinklist/qg1_2.c
@@ -5,40 +5,16 @@
 #include <conio.h>
 #define Len sizeof(struct event)    //Len为结构体的长度 
 typedef  struct event Event;        //重命名结构体
+#include"qg1_2.h"
 
-struct event {
-    struct event *front;
-    char data[100];
-    struct event *next;
-};
-
-Event *getnode(Event *duhead){
-    Event *follow, *new;
-    follow = new = (Event*)malloc(Len);
-
-    printf("Please intput data(enter only to exit):> ");
-    gets(new->data);
-    fflush(stdin);
-
-    while(strlen(new->data)){
-        
-        if(duhead == NULL){ 
-            new->front = NULL;
-            new->next = NULL;
-            duhead = new;
-        }else{
-            follow->next = new;  //连接新节点
-            new->front = follow; //反向连接旧节点
-            new->next = NULL;    //新节点next指针指向null
-            follow = follow->next; //follow指向新节点（便于新节点反向连接）
-        }
-        
-        new = (Event*)malloc(Len);
-        printf("Please intput data(enter only to exit):> ");
-        gets(new->data);
-        fflush(stdin);
+int main(){
+    Event *duhead,*read;
+    duhead = getnode(duhead = NULL);
+    read = duhead;
+    int nu = 1;
+    while(read){
+        printf("data NO.%d: ",nu++);
+        printf("%s\n",read->data);
+        read = read->next;
     }
-
-    free(new); //释放申请的冗余空间
-    return(duhead);
 }
