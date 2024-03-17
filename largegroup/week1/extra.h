ea7d1b9bcdab401f54781797f34d9fbd56995dc7
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-17 23:53:53 +0800

    Update extra.h

--- largegroup/week1/extra.h
+++ largegroup/week1/extra.h
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h> 
+#include <conio.h>
+#define Len sizeof(struct event)    //Len为结构体的长度 
+typedef  struct event Event;        //重命名结构体
 
