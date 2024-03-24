8fa8a0ae220d3470a235b042ea761509f85ed8b4
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-24 21:14:06 +0800

    Update Lqueue.h

--- largegroup/week2/Queue/Lqueue.h
+++ largegroup/week2/Queue/Lqueue.h
@@ -1,10 +1,4 @@
-#ifndef LQUEUE_H_INCLUDED
-#define LQUEUE_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <windows.h> 
-#include <conio.h>
+
 #define MAXQUEUE 10
 typedef struct Aqueue
 {
