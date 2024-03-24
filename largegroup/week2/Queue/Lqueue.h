108ba818381052760416be05a4422c73f0a783bb
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-24 21:13:37 +0800

    Update Lqueue.h

--- largegroup/week2/Queue/Lqueue.h
+++ largegroup/week2/Queue/Lqueue.h
@@ -5,3 +5,19 @@
 #include <string.h>
 #include <windows.h> 
 #include <conio.h>
+#define MAXQUEUE 10
+typedef struct Aqueue
+{
+    void *data[MAXQUEUE];      //数据域
+    int front;
+    int rear;
+   int length;        //队列长度
+} AQueue;
+
+typedef enum
+{
+    FALSE=0, TRUE=1
+} Status;
+char type;
+char datatype[MAXQUEUE];
+int pri; 
