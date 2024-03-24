7484aa454e135ff6a9587a7810bb70b49c681cc2
ChuHoiLam <163703655+ChuHoiLam@users.noreply.github.com>
2024-03-24 20:40:29 +0800

    Update LinkStack.c

--- largegroup/week2/Stack/LinkStack.c
+++ largegroup/week2/Stack/LinkStack.c
@@ -1,1 +1,23 @@
 
+
+#include'LinkStack.h'
+int main()
+{
+    char str[1024]={0};//将数组每个元素赋值为'\0'
+    printf("请输入四则运算表达式:\n");
+    scanf("%s",str);
+    int number=transform(str);
+    if(number==-1)
+    {
+        printf("表达式转换错误:\n");
+    }
+    else if(number==1)
+    {
+        printf("转化后的表达式: %s\n",buffer);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
